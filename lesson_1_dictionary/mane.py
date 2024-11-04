e4d7c8042a8331c65870afd8a3379c8a9e103c05
DogDogDogDogDogDogDogDogDogDog <oliverholloway@hotmail.com>
2024-11-04 19:58:35 +0000

    Add files via upload

---
+++ lesson_1_dictionary/mane.py
@@ -0,0 +1,10 @@
+'''for i in range(10000000):
+    print(i)
+    i=i+1'''
+#creating dictinary
+student={
+    'name':'Bobby mc Bobson BobbyBob',
+    'age':12.42149052123145798235469,
+    'subject':'Dogs'
+}
+print(student['subject'])
